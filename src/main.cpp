87a78f3c3e20aff2738faffe116a25ac64ffa35d
Carl <Carl.Kamholtz@gmail.com>
2020-12-12 13:59:52 +1000

    WIP from 6/12/2020

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,531 @@
 
-#include <Arduino.h>
 #include <IRremoteESP8266.h>
 #include <IRsend.h>
+
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h>
+#include <ArduinoOTA.h>
+#include <math.h>
 #include <ir_Coolix.h>
+#include <NTPClient.h>
 
-const uint16_t kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRCoolixAC ac(kIrLed);  // Set the GPIO to be used to sending the message.
+#define MQTT_MAX_PACKET_SIZE 512
+#define MQTT_SOCKET_TIMEOUT 1
 
-void setup() {
-  ac.begin();
-  Serial.begin(115200);
+#include "main.h"
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+// Global Variable
+const int OLED_ckl = D3;
+const int OLED_sdin = D2;
+const int OLED_rst = D1;
+const int OLED_cs = D0;
+WiFiClient espClient;
+WiFiManager wifiManager;
+PubSubClient client(espClient);
+IRCoolixAC ac(IR_pin);
+IRsend irsend(IR_pin);
+
+// Set up the NTP UDP client
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
+
+//Declare prototype functions
+void increment();
+void decrement();
+void update();
+void updateServerValue();
+
+void btnSwingInt()
+{
+  isSwing = !isSwing;
+  update();
+  updateServerValue();
 }
 
-void loop() {
-  Serial.println("Sending...");
+void btnSpeedInt()
+{
+  fanSpeed = ++fanSpeed % 4;
+  update();
+  updateServerValue();
+}
 
-  // Set up what we want to send. See ir_Argo.cpp for all the options.
-  ac.setPower(true);
-  ac.setFan(kCoolixFanAuto0);
-  ac.setMode(kCoolixCool);
-  ac.setTemp(23);
+// function for up-down interrupt
+void ICACHE_RAM_ATTR btnUpInt()
+{
+  currentAction = ACTION_UP;
+}
 
-#if SEND_ARGO
-  // Now send the IR signal.
-  ac.send();
-#else  // SEND_ARGO
-  Serial.println("Can't send because SEND_ARGO has been disabled.");
-#endif  // SEND_ARGO
+void ICACHE_RAM_ATTR btnDnInt()
+{
+  currentAction = ACTION_DOWN;
+}
 
-  delay(5000);
+void changeACmode()
+{
+  isCool = !isCool;
+  update();
+  updateServerValue();
+}
+
+void update()
+{
+  static bool isLastOn = false;
+  static int lastTemp = 0;
+  static bool lastIsSwing = isSwing;
+  static int lastFanSpeed = fanSpeed;
+  static int lastIsCool = isCool;
+
+  if (setTemp == lastTemp && isSwing == lastIsSwing && lastFanSpeed == fanSpeed && lastIsCool == isCool && isLastOn == isOn)
+    return;
+
+  if (isOn)
+  {
+    ac.on();
+    if (isCool)
+    {
+      Serial.println("Send IR : temp = " + String(setTemp) + " swing = " + String(isSwing) + " Fan Speed : " + String(fanSpeed) + " IsLastOn : " + isLastOn);
+      isLastOn = true;
+      lastTemp = setTemp;
+      lastIsSwing = isSwing;
+      lastFanSpeed = fanSpeed;
+      lastIsCool = true;
+
+      //Set and send commands
+      ac.setMode(kCoolixCool);
+      switch (fanSpeed)
+      {
+      case (0):
+        ac.setFan(kCoolixFanAuto);
+        break;
+      case (1):
+        ac.setFan(kCoolixFanMin);
+        break;
+      case (2):
+        ac.setFan(kCoolixFanMed);
+        break;
+      case (3):
+        ac.setFan(kCoolixFanMax);
+        break;
+      }
+
+      ac.setSwing();
+      ac.setTemp(setTemp);
+    }
+    else
+    {
+      ac.setMode(kCoolixFan);
+      lastIsCool = false;
+    }
+
+    ac.send();
+  }
+  else if (isLastOn)
+  {
+    Serial.println("Send off");
+    isLastOn = false;
+
+    //set and send command
+    ac.off();
+    irsend.sendRaw(ac_off, 349, 38); //I need to send raw for off because Library didn't work.
+  }
+
+  //Turn on Display
+  Serial.println("UpdateDisplay");
+  previousMillis = millis();
+  currentContrast = 255;
+}
+
+void updateServerValue()
+{
+
+  /*!!-- Need to redefine MQTT_MAX_PACKET_SIZE 256 --!! */
+
+  String value;
+  String message;
+  char data[200];
+
+  //Primary
+  // message = "{\"name\" : \"" + device_name + "\", \"service_name\" : \"" + service_name + "\", \"characteristic\" : \"CurrentTemperature\", \"value\" : " + String(setTemp) + "}";
+  // message.toCharArray(data, (message.length() + 1));
+  message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"CurrentTemperature\",\"value\":" + String(setTemp) + "}";
+  client.publish(mqtt_device_value_to_set_topic, message.c_str());
+
+  message = "{\"name\" : \"" + device_name + "\", \"service_name\" : \"" + service_name + "\", \"characteristic\" : \"Active\", \"value\" : " + String(isOn) + "}";
+  message.toCharArray(data, (message.length() + 1));
+  client.publish(mqtt_device_value_to_set_topic, data);
+
+  message = "{\"name\" : \"" + device_name + "\", \"service_name\" : \"" + service_name + "\", \"characteristic\" : \"SwingMode\", \"value\" : " + String(isSwing) + "}";
+  message.toCharArray(data, (message.length() + 1));
+  client.publish(mqtt_device_value_to_set_topic, data);
+
+  message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"CoolingThresholdTemperature\",\"value\":" + String(setTemp) + " }";
+  client.publish(mqtt_device_value_to_set_topic, message.c_str());
+
+  message = "{\"name\" : \"" + device_name + "\", \"service_name\" : \"" + service_name + "\", \"characteristic\" : \"RotationSpeed\", \"value\" : " + String(fanSpeed) + "}";
+  message.toCharArray(data, (message.length() + 1));
+  client.publish(mqtt_device_value_to_set_topic, data);
+
+  if (isCool)
+  {
+    message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"CurrentHeaterCoolerState\",\"value\":1}";
+    client.publish(mqtt_device_value_to_set_topic, message.c_str());
+    message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"TargetHeaterCoolerState\",\"value\":2}";
+    client.publish(mqtt_device_value_to_set_topic, message.c_str());
+  }
+  else
+  {
+    message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"CurrentHeaterCoolerState\",\"value\":2}";
+    client.publish(mqtt_device_value_to_set_topic, message.c_str());
+    message = "{\"name\": \"Smart AC\",\"service_name\": \"smart_ac\",\"characteristic\": \"TargetHeaterCoolerState\",\"value\":1}";
+    client.publish(mqtt_device_value_to_set_topic, message.c_str());
+  }
+
+  //Secondary
+  message = "{\"name\" : \"" + device_name_secondary + "\", \"service_name\" : \"" + service_name_secondary + "\", \"characteristic\" : \"On\", \"value\" : " + String(isOn) + "}";
+  message.toCharArray(data, (message.length() + 1));
+  client.publish(mqtt_device_value_to_set_topic, data);
+}
+
+void blink()
+{
+  //Blink on received MQTT message
+  digitalWrite(2, LOW);
+  delay(20);
+  digitalWrite(2, HIGH);
+}
+
+void setup_ota()
+{
+  // Set OTA Password, and change it in platformio.ini
+  ArduinoOTA.setHostname("ESP8266-AC");
+  // ArduinoOTA.setPassword("12345678");
+  ArduinoOTA.onStart([]() {});
+  ArduinoOTA.onEnd([]() {});
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {});
+  ArduinoOTA.onError([](ota_error_t error) {
+    if (error == OTA_AUTH_ERROR)
+      ; // Auth failed
+    else if (error == OTA_BEGIN_ERROR)
+      ; // Begin failed
+    else if (error == OTA_CONNECT_ERROR)
+      ; // Connect failed
+    else if (error == OTA_RECEIVE_ERROR)
+      ; // Receive failed
+    else if (error == OTA_END_ERROR)
+      ; // End failed
+  });
+  ArduinoOTA.begin();
+}
+
+void ACOnOff()
+{
+  if (!isOn)
+  {
+    Serial.println("AC On");
+    isOn = true;
+  }
+  else
+  {
+    Serial.println("AC Off");
+    isOn = false;
+  }
+  update();
+}
+
+void reconnect()
+{
+  // Create a random client ID
+  String clientId = "ESP8266Client-";
+  clientId += String(random(0xffff), HEX);
+  // Attempt to connect
+
+  if (client.connect(clientId.c_str()))
+  {
+    // Once connected, resubscribe.
+    client.subscribe(mqtt_device_value_from_set_topic);
+  }
+}
+
+void increment()
+{
+  if (!isCool)
+    return;
+  setTemp++;
+  update();
+  updateServerValue();
+}
+
+void decrement()
+{
+  if (!isCool)
+    return;
+  setTemp--;
+  update();
+  updateServerValue();
+}
+
+void power()
+{
+  ACOnOff();
+  updateServerValue();
+}
+
+void ICACHE_RAM_ATTR btnPowerPressed()
+{
+  // int press_millis = millis();
+  // while (digitalRead(btnPower) == LOW)
+  // {
+  //   delay(0);
+  //   if (millis() - press_millis > 1000)   //if long press, change AC Mode
+  //   {
+  //     isCool = !isCool;
+  //     update();
+  //     return;
+  //   }
+  // };
+  currentAction = ACTION_POWER;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char c_payload[length];
+  memcpy(c_payload, payload, length);
+  c_payload[length] = '\0';
+
+  String s_topic = String(topic);
+  String s_payload = String(c_payload);
+
+  Serial.println(s_payload + "\0");
+
+  StaticJsonBuffer<200> jsonBuffer;
+
+  JsonObject &root = jsonBuffer.parseObject(s_payload);
+
+  const char *name = root["name"];
+
+  // Serial.println(name);
+  if (strcmp(name, device_name.c_str()) != 0 && strcmp(name, device_name_secondary.c_str()) != 0)
+  {
+    return;
+  }
+
+  // blink();
+  const char *characteristic = root["characteristic"];
+
+  if (strcmp(characteristic, "CoolingThresholdTemperature") == 0)
+  {
+    int value = root["value"];
+    if (value < minTemp || value > maxTemp)
+    {
+      return;
+    }
+    setTemp = value;
+    update();
+  }
+  if (strcmp(characteristic, "Active") == 0)
+  {
+    int value = root["value"];
+    if (value != 1 && value != 0)
+    {
+      return;
+    }
+    isOn = value;
+    update();
+
+    //set secondary device status (For primitive service as regular fan.)
+    char data[150];
+    String message = "{\"name\" : \"" + device_name_secondary + "\", \"service_name\" : \"" + service_name_secondary + "\", \"characteristic\" : \"On\", \"value\" : " + String(isOn) + "}";
+    message.toCharArray(data, (message.length() + 1));
+    client.publish(mqtt_device_value_to_set_topic, data);
+  }
+  if (strcmp(characteristic, "On") == 0)
+  {
+    int value = root["value"];
+    if (value != 1 && value != 0)
+    {
+      return;
+    }
+    isOn = value;
+    update();
+
+    //set primary device status (For Homekit HeaterCooler service)
+    char data[150];
+    String message = "{\"name\" : \"" + device_name + "\", \"service_name\" : \"" + service_name + "\", \"characteristic\" : \"Active\", \"value\" : " + String(isOn) + "}";
+    message.toCharArray(data, (message.length() + 1));
+    client.publish(mqtt_device_value_to_set_topic, data);
+  }
+
+  if (strcmp(characteristic, "SwingMode") == 0)
+  {
+    int value = root["value"];
+    if (value != 1 && value != 0)
+    {
+      return;
+    }
+    isSwing = value;
+    update();
+  }
+  if (strcmp(characteristic, "RotationSpeed") == 0)
+  {
+    int value = root["value"];
+    if (value < 0 || value > 3)
+    {
+      return;
+    }
+    fanSpeed = value;
+    update();
+  }
+  /*
+    // The value property of TargetHeaterCoolerState must be one of the following:
+  Characteristic.TargetHeaterCoolerState.AUTO = 0;
+  Characteristic.TargetHeaterCoolerState.HEAT = 1;
+  Characteristic.TargetHeaterCoolerState.COOL = 2;
+  */
+  if (strcmp(characteristic, "TargetHeaterCoolerState") == 0)
+  {
+    int value = root["value"];
+    if (value < 0 || value > 3)
+    {
+      return;
+    }
+    if (value == 0 || value == 2)
+    {
+      isCool = true;
+    }
+    else
+    {
+      isCool = false;
+    }
+    update();
+  }
+
+  updateServerValue();
+}
+
+void autoAdjustScreenBrightness()
+{
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= screenBrightnessUpdateInt)
+  {
+
+    // Serial.println(analogRead(ldrPin));
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    if (analogRead(ldrPin) < 85 && currentContrast != 0)
+    {
+      Serial.println("Clear");
+    }
+    else if (analogRead(ldrPin) < 400 && currentContrast != 1 || !isOn)
+    {
+      currentContrast = 1;
+    }
+    else if (analogRead(ldrPin) >= 400)
+    {
+      // u8g2.setContrast(1);
+
+      currentContrast = 255;
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  //Setup display
+  Serial.println("Setting up display");
+
+  // Setup buttons
+  Serial.println("Setting up buttons");
+  pinMode(btnPower, INPUT_PULLUP);
+  pinMode(btnUp, INPUT_PULLUP);
+  pinMode(btnDn, INPUT_PULLUP);
+
+  pinMode(2, OUTPUT);
+
+  // Setup networking
+  Serial.println("Setting up network");
+  wifiManager.setConfigPortalTimeout(120);
+  wifiManager.autoConnect(autoconf_ssid, autoconf_pwd);
+  setup_ota();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  //Attach interrupt for manual button controls
+  Serial.println("Attaching Interupt");
+  attachInterrupt(digitalPinToInterrupt(btnPower), btnPowerPressed, FALLING);
+  attachInterrupt(digitalPinToInterrupt(btnUp), btnUpInt, FALLING);
+  attachInterrupt(digitalPinToInterrupt(btnDn), btnDnInt, FALLING);
+
+  digitalWrite(2, HIGH);
+  updateServerValue();
+
+  //Setup IR Lib
+  Serial.println("Setting up IR Lib");
+  ac.begin();
+  irsend.begin();
+
+  //Start the NTP UDP client
+  Serial.println("Setting up NTP Client");
+  timeClient.begin();
+
+  Serial.println("Setup done!");
+}
+
+void handleCurrentAction()
+{
+
+  switch (currentAction)
+  {
+  case ACTION_UP:
+  {
+    increment();
+    break;
+  }
+  case ACTION_DOWN:
+  {
+    decrement();
+    break;
+  }
+  case ACTION_POWER:
+  {
+    power();
+    break;
+  }
+  default:
+  {
+    break;
+  }
+  }
+
+  currentAction = 0;
+}
+
+void loop()
+{
+  if (!client.connected() && (millis() - lastConnectingTime > 60000))
+  {
+    Serial.println("reconnect");
+    reconnect();
+    lastConnectingTime = millis();
+  }
+  client.loop();
+  ArduinoOTA.handle();
+  autoAdjustScreenBrightness();
+  handleCurrentAction();
 }
---
+++ src/main.h
@@ -0,0 +1,73 @@
+#include <Arduino.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+
+{
+"name": "Smart AC",
+"Active": 1,
+"CurrentHeatingCoolingState" : 3,
+"TargetHeaterCoolerState": 2,
+"CurrentTemperature": 26,
+
+"service_name" : "smart_ac",
+"service": "HeaterCooler",
+"CoolingThresholdTemperature": { "maxValue": 28, "minValue": 23, "minStep": 1},
+"SwingMode" : 1,
+"RotationSpeed":  { "maxValue": 3, "minValue": 0, "minStep": 1}
+}
+*/
+
+// Constants
+const char *autoconf_ssid = "IPF";		 //AP name for WiFi setup AP which your ESP will open when not able to connect to other WiFi
+const char *autoconf_pwd = "fuckna1234"; //AP password so noone else can connect to the ESP in case your router fails
+const char *mqtt_server = "192.168.1.5"; //MQTT Server IP, your home MQTT server eg Mosquitto on RPi, or some public MQTT
+const int mqtt_port = 1883;				 //MQTT Server PORT, default is 1883 but can be anything.
+const int btnPower = D5;
+const int btnUp = D6;
+const int btnDn = D7;
+const int btnSwing = 3;
+const int btnSpeed = 1;
+const int ldrPin = A0;
+const int IR_pin = D2;
+
+const int screenBrightnessUpdateInt = 5000; //Interval time for adjusting screen's brightness according to environment light.
+
+// MQTT Constants
+const char *mqtt_device_value_from_set_topic = "homebridge/from/set";
+const char *mqtt_device_value_to_set_topic = "homebridge/to/set";
+String device_name = "Smart AC";
+String service_name = "smart_ac";
+String device_name_secondary = "Smart AC Basic";
+String service_name_secondary = "smart_ac_basic";
+
+// Global variables
+unsigned int isOn = false;
+unsigned int setTemp = 23;
+unsigned int isCool = true; //false = fan mode
+unsigned int fanSpeed = 0;	//0: Auto, 1:Min, 2:Medium, 3:High
+unsigned int maxTemp = 28;
+unsigned int minTemp = 18;
+unsigned int currentAction = 0;
+bool isSwing = false;
+bool rotary_1 = false;
+bool rotary_2 = false;
+unsigned long previousMillis = 0;	  //For auto screen brightness
+unsigned long lastConnectingTime = 0; //For auto screen brightness
+int currentContrast = 255;
+
+// Define NTP properties
+#define NTP_OFFSET 7 * 60 * 60	   // In seconds
+#define NTP_INTERVAL 60 * 1000	   // In miliseconds
+#define NTP_ADDRESS "ntp.ku.ac.th" // change this to whatever pool is closest (see ntp.org)
+
+#define ACTION_POWER 1
+#define ACTION_UP 2
+#define ACTION_DOWN 3
+
+//AC off raw data, because I can't turn off my AC with IRSamsungAc library
+uint16_t ac_off[349] = {640, 17770, 3056, 8898, 522, 472, 550, 1436, 554, 438, 560, 434, 560, 434, 560, 432, 556, 438, 554, 438, 554, 438, 554, 1432, 556, 442, 550, 442, 550, 1436, 550, 1458, 474, 520, 516, 1470, 552, 1436, 556, 1428, 560, 1428, 584, 1402, 560, 438, 556, 438, 554, 438, 556, 438, 556, 438, 554, 438, 554, 442, 552, 464, 528, 464, 470, 524, 542, 450, 546, 446, 552, 442, 554, 438, 556, 438, 556, 434, 558, 434, 560, 434, 560, 434, 556, 438, 556, 442, 552, 442, 550, 444, 550, 442, 550, 448, 546, 464, 528, 464, 474, 518, 546, 446, 550, 442, 554, 438, 554, 438, 560, 434, 560, 434, 554, 1432, 556, 1432, 556, 2934, 3030, 8898, 556, 1436, 550, 442, 550, 468, 526, 464, 530, 464, 474, 520, 546, 446, 550, 442, 556, 438, 554, 1432, 554, 438, 554, 440, 554, 1432, 556, 438, 556, 1432, 554, 1436, 552, 1436, 550, 1458, 554, 1432, 474, 1514, 550, 442, 550, 444, 554, 438, 554, 438, 554, 438, 554, 438, 554, 438, 556, 438, 556, 438, 556, 438, 554, 442, 550, 442, 550, 442, 552, 464, 530, 464, 530, 464, 470, 522, 546, 448, 550, 442, 550, 444, 554, 438, 554, 438, 556, 438, 556, 438, 554, 438, 550, 442, 550, 442, 552, 442, 550, 442, 550, 442, 550, 446, 546, 468, 524, 470, 524, 468, 516, 476, 548, 446, 550, 2912, 3052, 8900, 554, 1434, 554, 438, 554, 438, 556, 438, 556, 438, 556, 438, 554, 438, 554, 442, 550, 442, 550, 1438, 550, 464, 530, 464, 474, 1514, 546, 446, 550, 442, 550, 442, 552, 1432, 554, 1432, 554, 1432, 554, 1432, 556, 442, 550, 1436, 552, 442, 550, 1458, 530, 1458, 520, 478, 546, 448, 546, 446, 550, 1434, 554, 1432, 556, 1432, 556, 438, 530, 464, 528, 470, 524, 468, 520, 474, 520, 1466, 522, 472, 522, 494, 500, 1488, 520, 1468, 550, 444, 550, 442, 526, 468, 524, 1462, 524, 468, 526, 468, 526, 468, 526, 468, 526, 468, 524, 474, 520, 472, 520, 472, 522, 498, 494, 1488, 500, 1488, 526}; // SAMSUNG_AC
+
+#endif
